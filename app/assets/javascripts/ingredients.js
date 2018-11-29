8b7ea263608096d06eff7c22cb0486bd1cfc3fbc
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-29 11:11:41 -0500

    working on AJAX for ingredients form

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,6 +1,33 @@
+// $(function(){
+//   $("#new_ingredient").on("submit", function(e){
+//     $.ajax({
+//     type: ($("input[name='_method']").val() || this.method),
+//     url: this.action,
+//     data: $(this).serialize();,
+//     success: function(response){
+//       $("#ingredient_name").val("");
+//       var $ol = $("div.ingredients ol")
+//       $ol.append(response);
+//     }
+//   });
+//     e.preventDefault();
+//   })
+// });
+
 $(function(){
-  $("#new_ingredient").on("submit", function(){
-    alert("test")
-    // e.preventDefault();
+  $("#new_ingredient").on("submit", function(e){
+
+    $.ajax({
+      type: ($("input[name='_method']").val() || this.method),
+      url: this.action,
+      data: $(this).serialize(),
+      success: function(response){
+        $("#ingredient_name").val("");
+        var $ol = $("div.ingredients ol")
+        $ol.append(response);
+      }
+    });
+
+    e.preventDefault();
   })
-})
+});
