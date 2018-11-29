29317834326fa2498678e5f55add77daef5ffea9
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-29 10:35:39 -0500

    successfully hijacked click event on ingredient submittal

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,20 +1,6 @@
-$("form").submit(function){
-  alert("test")
-  e.preventDefault();
-  e.returnValue = false;
-
+$(function(){
+  $("#new_ingredient").on("submit", function(){
+    alert("test")
+    // e.preventDefault();
+  })
 })
-
-// $("form").on('submit', function(e) {
-//   alert("test")
-//   e.preventDefault();
-//   e.returnValue = false;
-//
-// })
-
-
-
-// $("a.add_ingredient").on("click", function(e){
-//   alert("test")
-//   e.preventDefault();
-// })
